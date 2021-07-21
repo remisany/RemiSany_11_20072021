b0746de1f304ad97cc4c64db5b468c57cf7dc058
remisany <remi.d.sany@gmail.com>
2021-07-21 18:16:57 +0200

    Ajout composant Error + page 404

---
+++ kasa-app/src/components/Error.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+import "../styles/Error.css"
+
+class Error extends React.Component {
+    render() {
+        return (
+            <main className = "k-error">
+                <h1 className = "k-error-title">404</h1>
+                <h2 className = "k-error-tagline">Oups! La page que vous demandez n'existe pas</h2>
+                <Link className = "k-error-link" to = "/">Retourner sur la page d'accueil</Link>
+            </main>
+        )
+    }
+}
+
+export default Error
