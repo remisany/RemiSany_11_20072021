933ed46270797343bb32086db7262e51da50d7ec
remisany <remi.d.sany@gmail.com>
2021-07-22 16:42:21 +0200

    Ajout composant Card

---
+++ kasa-app/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+import "../styles/Card.css"
+
+class Card extends React.Component {
+    render() {
+
+        const {id, title, cover} = this.props
+
+        return (
+            <Link to = {`/accomodation/${id}`} className = "k-card">
+                <div className = "k-card-cover"></div>
+                <img src = {cover} alt = {title} className = "k-card-img"/>
+                <p className = "k-card-title">{title}</p>
+            </Link>
+        )
+    }
+}
+
+export default Card
--- kasa-app/src/pages/Home.jsx
+++ kasa-app/src/pages/Home.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 
 import Banner from "../components/Banner";
+import Card from "../components/Card";
 
 import banner from "../assets/BannerHome.png"
+
+import { accomodationList } from "../datas/accomodationList";
+
+import "../styles/Home.css"
 
 class Home extends React.Component {
   render () {
     return (
-      <React.Fragment>
+      <main>
         <Banner
           content = "Chez vous, partout et ailleurs"
           src = {banner}
           alt = "Photo de la bannière"
         />
-      </React.Fragment>
+        <section className = "k-card-container">
+            {accomodationList.map((accomodation) => (
+              <Card
+              id = {accomodation.id}
+              title = {accomodation.title}
+              cover = {accomodation.cover}
+              />
+            ))}
+        </section>
+      </main>
     );
   }
 }
