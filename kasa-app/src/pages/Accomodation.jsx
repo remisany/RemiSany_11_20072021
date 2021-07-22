7f4b73cdf5ce8b1964772bdaea115e024f460181
remisany <remi.d.sany@gmail.com>
2021-07-22 17:56:51 +0200

    Ajout main

--- kasa-app/src/pages/Accomodation.jsx
+++ kasa-app/src/pages/Accomodation.jsx
@@ -3,8 +3,8 @@ import React from "react";
 class Accomodation extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <main>
+      </main>
     );
   }
 }
