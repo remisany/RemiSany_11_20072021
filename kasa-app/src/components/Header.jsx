383b44082a1b69c6c9f937f57c678a3eea3ff634
remisany <remi.d.sany@gmail.com>
2021-07-27 18:39:12 +0200

    Ajout lien souligné

--- kasa-app/src/components/Header.jsx
+++ kasa-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 //React
 import React from "react";
-import {Link} from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 
 //Assets
 import logo from "../assets/Logo.svg"
@@ -12,10 +12,10 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <Link to = "/"><img src={logo} alt="Kasa" className="k-logo"/></Link>
+                <Link to = "/"><img src={logo} alt="Logo de Kasa" className="k-logo"/></Link>
                 <nav className = "k-nav">
-                    <Link to = "/" className = "k-nav-link">accueil</Link>
-                    <Link to = "/about" className = "k-nav-link">à propos</Link>
+                    <NavLink exact to = "/" className = "k-nav-link" activeStyle = {{ textDecoration: "underline" }}>accueil</NavLink>
+                    <NavLink to = "/about" className = "k-nav-link" activeStyle = {{ textDecoration: "underline" }}>à propos</NavLink>
                 </nav>
             </header>
         )
